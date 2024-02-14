a37a48c91ebf6ee37d97dc0f310f99f0101c8b46
ct <charleswtiffany@gmail.com>
2024-02-14 14:42:38 -0800

    fixed handlebars/tailwind bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['main.handlebars'],
+  content: ['./views/layouts/main.handlebars'],
   theme: {
     extend: {
       fontFamily: {
