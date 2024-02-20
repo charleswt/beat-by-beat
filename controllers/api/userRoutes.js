76bcd0dfb54be53c2bda71b7f6363c5cae8bcd2b
Lucy Kwon <ks02ks222@gmail.com>
2024-02-20 15:32:40 -0800

    changed how to render error game

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,82 +1,99 @@
-const router = require('express').Router();
-const { User } = require('../../models');
-const { Op } = require('sequelize');
+const router = require("express").Router();
+const { User } = require("../../models");
+const { Op } = require("sequelize");
 
-router.post('/', async (req, res) => {
-
+router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      res.json({message: "logged in"});
+      res.json({ message: "logged in" });
     });
-    
   } catch (err) {
     //if the error involves a unique constraint violation,
-    if (err.name === 'SequelizeUniqueConstraintError') {
+    if (err.name === "SequelizeUniqueConstraintError") {
       //check if the error occurs at the email field
-      const isEmail = err.errors[0].path === 'email';
-      //if isEmail, send a message 
-      res.status(409).json({ message: isEmail ? 'Email already in use, please choose another.' : 'Username already taken, please choose another.' });
-    } else if (err.name === 'SequelizeValidationError') {
+      const isEmail = err.errors[0].path === "email";
+      //if isEmail, send a message
+      res
+        .status(409)
+        .json({
+          message: isEmail
+            ? "Email already in use, please choose another."
+            : "Username already taken, please choose another.",
+        });
+    } else if (err.name === "SequelizeValidationError") {
       // Find out if the error is related to the password field
-      const isPasswordError = err.errors.some(error => error.path === 'password');
+      const isPasswordError = err.errors.some(
+        (error) => error.path === "password"
+      );
       if (isPasswordError) {
-        return res.status(400).json({ message: 'Password needs to be at least 8 character' });
-      } 
-    }else {
-      res.render('game', { layout: 'error' });
+        return res
+          .status(400)
+          .json({ message: "Password needs to be at least 8 character" });
+      }
+    } else {
+      const statusCode = 400;
+      // Render your template and pass the status code as part of the data object
+      res.status(statusCode).render("game", {
+        layout: "error",
+        status: statusCode,
+      });
     }
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({
       where: {
-        [Op.or]: [
-          { email: req.body.email },
-          { name: req.body.email }
-        ]
-      }
+        [Op.or]: [{ email: req.body.email }, { name: req.body.email }],
+      },
     });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email/username, please try again' });
+        .json({ message: "Incorrect email/username, please try again" });
       return;
     }
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect password, please try again' });
+      res.status(400).json({ message: "Incorrect password, please try again" });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
 
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
   } catch (err) {
-   res.render('game', { layout: 'error' }).status(400).json(err);
+    const statusCode = 400;
+    // Render your template and pass the status code as part of the data object
+    res.status(statusCode).render("game", {
+      layout: "error",
+      status: statusCode,
+    });
   }
 });
 
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
-    res.render('game', { layout: 'error' })
+    const statusCode = 400;
+    // Render your template and pass the status code as part of the data object
+    res.status(statusCode).render("game", {
+      layout: "error",
+      status: statusCode,
+    });
   }
 });
 
